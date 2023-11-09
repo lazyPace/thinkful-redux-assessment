9ae43e1faedf8ef834133a3d36e11c63dc9aa150
lazyPace <brandonprater84@yahoo.com>
2023-11-09 17:41:24 -0500

    add/remove photo feature complete

--- src/features/photos/create/index.js
+++ src/features/photos/create/index.js
@@ -1,53 +1,56 @@
-import { useState } from 'react';
-
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addPhoto } from '../photos.slice'
 // Task 2: Import the `useDispatch()` method from the appropriate package
 // Task 3: Import the `addPhoto()` action creator from the photos slice
 
-import './create.css';
+import './create.css'
 
-export default function CreatePhoto() {
-  const [formData, setFormData] = useState({ imageUrl: '', caption: '' });
+export default function CreatePhoto () {
+  const [formData, setFormData] = useState({ imageUrl: '', caption: '' })
   // Task 4: Store a reference to the Redux store's dispatch method in a variable called `dispatch`
+  const dispatch = useDispatch()
 
-  function handleChange({ target: { name, value } }) {
+  function handleChange ({ target: { name, value } }) {
     setFormData({
       ...formData,
-      [name]: value,
-    });
+      [name]: value
+    })
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  function handleSubmit (event) {
+    event.preventDefault()
     // Task 5: Dispatch the `addPhoto()` action creator, passing in the form data
-    setFormData({ imageUrl: '', caption: '' });
+    dispatch(addPhoto(formData))
+    setFormData({ imageUrl: '', caption: '' })
   }
 
   return (
-    <form className="create-form" onSubmit={handleSubmit}>
+    <form className='create-form' onSubmit={handleSubmit}>
       <h2>Add a dog</h2>
       <div>
-        <label htmlFor="url">Enter your image's url: </label>
+        <label htmlFor='url'>Enter your image's url: </label>
         <input
-          id="url"
-          name="imageUrl"
+          id='url'
+          name='imageUrl'
           onChange={handleChange}
-          placeholder="e.g., https://images.dog.ceo/breeds/australian-shepherd/pepper.jpg"
-          type="text"
+          placeholder='e.g., https://images.dog.ceo/breeds/australian-shepherd/pepper.jpg'
+          type='text'
           value={formData.imageUrl}
         />
       </div>
       <div>
-        <label htmlFor="caption">Enter your image's caption: </label>
+        <label htmlFor='caption'>Enter your image's caption: </label>
         <input
-          id="caption"
-          name="caption"
+          id='caption'
+          name='caption'
           onChange={handleChange}
-          placeholder="e.g., Australian Shepherd"
-          type="text"
+          placeholder='e.g., Australian Shepherd'
+          type='text'
           value={formData.caption}
         />
       </div>
-      <input type="submit" />
+      <input type='submit' />
     </form>
-  );
+  )
 }
--- src/features/photos/list/index.js
+++ src/features/photos/list/index.js
@@ -1,18 +1,21 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux'
 import {
   // Task 7: Import the `removePhoto()` action creator from the photos slice
-  selectAllPhotos,
+  removePhoto,
+  selectAllPhotos
   // Task 13: Import the `selectFilteredPhotos()` selector from the photos slice
-} from '../photos.slice';
-import './list.css';
+} from '../photos.slice'
+import './list.css'
 
-export default function PhotosList() {
+export default function PhotosList () {
   // Task 14: Call `useSelector()` below with `selectFilteredPhotos` instead of `selectAllPhotos`
-  const photos = useSelector(selectAllPhotos);
+  const photos = useSelector(selectAllPhotos)
   // Task 8: Store a reference to the Redux store's dispatch method in a variable called `dispatch`
+  const dispatch = useDispatch()
 
-  function handleDeleteButtonClick(id) {
+  function handleDeleteButtonClick (id) {
     // Task 9: Dispatch the `removePhoto()` action creator, passing in the id
+    dispatch(removePhoto(id))
   }
 
   const photosListItems = photos.map(({ id, caption, imageUrl }) => (
@@ -22,16 +25,17 @@ export default function PhotosList() {
         <p>{caption}</p>
         <button
           data-testid={`${caption}-button`}
-          onClick={() => handleDeleteButtonClick(id)}>
+          onClick={() => handleDeleteButtonClick(id)}
+        >
           Delete
         </button>
       </div>
     </li>
-  ));
+  ))
 
   return photosListItems.length > 0 ? (
     <ul>{photosListItems}</ul>
   ) : (
     <h3>No doggies to display...</h3>
-  );
+  )
 }
